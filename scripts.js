0ee7f803a215de954d74d4a6c34fa5e636e7934e
redmix-x <shubhamadigantla113@gmail.com>
2024-07-20 18:37:25 +0530

    Delete scripts.js

--- scripts.js
+++
@@ -1,8 +0,0 @@
-// Mobile menu toggle
-const menuToggle = document.getElementById('mobile-menu');
-const nav = document.querySelector('header nav');
-
-menuToggle.addEventListener('click', () => {
-    menuToggle.classList.toggle('active');
-    nav.classList.toggle('active');
-});
