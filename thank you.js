66c6495ac8f6d0519f333ebe947bc266bb0cbceb
redmix-x <shubhamadigantla113@gmail.com>
2024-07-20 18:37:53 +0530

    Delete thank you.js

--- thank you.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('section#contact form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    // Simulate form submission
-    setTimeout(() => {
-        window.location.href = 'thank-you.html';
-    }, 500);
-});
